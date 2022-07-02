b6ec2a36c9fc56bf7de271227e5b71b3662b43fe
Saif Shaikh <saifshaikh1805@gmail.com>
2022-07-02 22:03:17 +0530

    fix: missing html attribute

--- pages/index.js
+++ pages/index.js
@@ -294,7 +294,7 @@ export default function Home() {
           <Grid.Row>
             <Grid.Column></Grid.Column>
             <Grid.Column textAlign='center' computer={8} tablet={12} mobile={16}>
-              <a href="https://github.com/saifshaikh1805/tata-sky-m3u" target="_blank">View source code on Github</a>
+              <a href="https://github.com/saifshaikh1805/tata-sky-m3u" target="_blank" rel="noreferrer">View source code on Github</a>
             </Grid.Column>
             <Grid.Column></Grid.Column>
           </Grid.Row>
