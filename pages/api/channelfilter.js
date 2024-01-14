56613fe58b13dbfc90d4e83a8ae7cf2642f836fa
Your <you@example.com>
2024-01-15 02:21:24 +0530

    Revert "Create channelfilter.js"
    This reverts commit f0f14aa4fc1c8133acc02c5da65b09eedf37a6fc.

--- pages/api/channelfilter.js
+++
@@ -1,7 +0,0 @@
-const skipChannelIds = [632,840];
-
-const filterChannels = (channels) => {
-    return channels.filter(channel => !skipChannelIds.includes(channel.channelMeta.id));
-};
-
-module.exports = { filterChannels };
